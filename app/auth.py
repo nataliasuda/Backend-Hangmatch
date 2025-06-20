34596d05bb659e2ef092c16bf6f9256d25909ac6
nataliasuda <natalia.suda7@wp.pl>
2025-06-20 16:16:58 +0200

    feat: add function to generate JWT access token

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+SECRET_KEY = "super-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +14,12 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
