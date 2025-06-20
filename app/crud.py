166bc77c2974165737c1de4d2f6df2246aee7a3f
nataliasuda <natalia.suda7@wp.pl>
2025-06-20 02:03:00 +0200

    feat: implement user CRUD operations

---
+++ app/crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from app import models, schemas, auth
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserRegister):
+    if user.password != user.repeated_password:
+        return None
+
+    hashed_password = auth.get_password_hash(user.password)
+    db_user = models.User(
+        name=user.name,
+        email=user.email,
+        password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user or not auth.verify_password(password, user.password):
+        return None
+    return user
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
