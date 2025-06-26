1f78b16860684abf9af82f34b10720280340b787
nataliasuda <natalia.suda7@wp.pl>
2025-06-27 00:53:40 +0200

    feat: implement session creation and retrieval

--- app/crud.py
+++ app/crud.py
@@ -30,3 +30,24 @@ def get_user(db: Session, user_id: int):
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_session(db: Session, session_data: schemas.SessionCreate, owner_id: int):
+    invited_users = db.query(models.User).filter(models.User.id.in_(session_data.invited_users_ids)).all()
+    new_session = models.Session(
+        name=session_data.name,
+        location_radius=session_data.location_radius,
+        owner_id=owner_id,
+        invited_users=invited_users
+    )
+    db.add(new_session)
+    db.commit()
+    db.refresh(new_session)
+    return new_session
+
+def get_sessions_for_user(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        return []
+
+    return user.owned_sessions + user.invited_sessions
