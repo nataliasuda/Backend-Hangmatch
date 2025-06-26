e9ea1724ae3e9746ca12166079f4e231544a49a6
nataliasuda <natalia.suda7@wp.pl>
2025-06-27 00:51:38 +0200

    feat: add Session model with owner and invited users relationships

--- app/models.py
+++ app/models.py
@@ -1,5 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from app.database import Base
+
+session_user_association = Table(
+    'session_user',
+    Base.metadata,
+    Column('session_id', Integer, ForeignKey('sessions.id')),
+    Column('user_id', Integer, ForeignKey('users.id'))
+)
 
 class User(Base):
     __tablename__ = "users"
@@ -7,3 +15,15 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String, nullable=False)
+    invited_sessions = relationship("Session", secondary=session_user_association, back_populates="invited_users")
+    owned_sessions = relationship("Session", back_populates="owner")
+
+class Session(Base):
+    __tablename__ = "sessions"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    location_radius = Column(Integer, nullable=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("User", back_populates="owned_sessions")
+    invited_users = relationship("User", secondary=session_user_association, back_populates="invited_sessions")
