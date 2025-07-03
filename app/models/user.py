3688318b41a2ab75f2c66bbc16389e2067259c26
nataliasuda <natalia.suda7@wp.pl>
2025-07-03 22:41:49 +0200

    feat: add friend request relationships in User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
@@ -7,3 +8,6 @@ class User(Base):
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String, nullable=False)
+
+    sent_friend_requests = relationship("FriendRequest", back_populates="sender", foreign_keys='FriendRequest.sender_id')
+    received_friend_requests = relationship("FriendRequest", back_populates="receiver", foreign_keys='FriendRequest.receiver_id')
