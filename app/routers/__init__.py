6da68f4c76a830f17562e9a0a123dd305d8c7344
nataliasuda <natalia.suda7@wp.pl>
2025-07-03 22:50:56 +0200

    chore: add imports and friends router export in __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from . import user
+from . import friend
 
 router = APIRouter()
 router.include_router(user.router, tags=["users"])
+router.include_router(friend.router, tags=["friends"])
