db9bbd5328a48ef57b108df900d46386b3bc7150
nataliasuda <natalia.suda7@wp.pl>
2025-07-16 16:07:40 +0200

    refactor: change IDs in session schemas from int to str

--- app/schemas/session.py
+++ app/schemas/session.py
@@ -1,19 +1,18 @@
 from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel
-import uuid
 
 class SessionCreate(BaseModel):
     name: str
     location_radius: int
-    invited_users_ids: List[int]
+    invited_users_ids: List[str]
 
 class SessionOut(BaseModel):
     id: str
     name: str
     location_radius: int
-    owner_id: int
-    invited_user_ids: Optional[List[int]] = []
+    owner_id: str
+    invited_user_ids: Optional[List[str]] = []
     created_at: datetime
 
     class Config:
